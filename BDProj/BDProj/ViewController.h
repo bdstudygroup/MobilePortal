8f01ad98db5c047c1d1c3cfb48de069cc506a095
Xiangyu Qu <771744189@qq.com>
2019-04-24 23:05:17 +0800

    创建空项目

---
+++ BDProj/BDProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BDProj
+//
+//  Created by apple on 2019/4/24.
+//  Copyright © 2019 Chauncey Qu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BDProj/BDProj/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BDProj
+//
+//  Created by apple on 2019/4/24.
+//  Copyright © 2019 Chauncey Qu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
